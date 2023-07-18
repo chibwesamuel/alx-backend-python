aab978151714af31e446c9a86d23b0feb23e2ade
Samuel Chibwe <smchibwe@gmail.com>
2023-07-18 19:44:35 +0000

    My fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,13 +4,19 @@
 import asyncio
 from typing import List
 
+# Import task_wait_random function
+from my_module import task_wait_random
+
+
 def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Creates and returns a list of asyncio.Tasks that execute task_wait_random with the specified max_delay.
+    Creates and returns a list of asyncio.Tasks that execute task_wait_random
+    with the specified max_delay.
 
     Args:
         n (int): The number of tasks to create.
-        max_delay (int): The maximum delay value in seconds for task_wait_random.
+        max_delay (int): The maximum delay value in seconds for
+        task_wait_random.
 
     Returns:
         List[float]: The list of delays (float values) in ascending order.
@@ -18,9 +24,12 @@ def task_wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     return asyncio.run(asyncio.gather(*tasks))
 
+
 if __name__ == "__main__":
-    task_wait_random = __import__('3-tasks').task_wait_random
+    # Import task_wait_random function
+    from my_module import task_wait_random
 
     n = 5
     max_delay = 6
     print(task_wait_n(n, max_delay))
+
