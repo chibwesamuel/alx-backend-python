aa26e99a79617f5052da48e6cfd4297a4a897fd1
Samuel Chibwe <smchibwe@gmail.com>
2023-07-18 15:38:56 +0000

    No more errors

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,6 +2,24 @@
 """Test file for printing the correct output of the wait_n coroutine"""
 import asyncio
 from typing import List
+from random import random
+import datetime
+
+
+async def wait_random(max_delay: int) -> float:
+    """
+    Asynchronous routine that waits for a random delay between 0 and max_delay.
+
+    Args:
+        max_delay (int): The maximum delay value in seconds.
+
+    Returns:
+        float: The random delay value.
+    """
+    delay = random() * max_delay
+    await asyncio.sleep(delay)
+    return delay
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
@@ -18,9 +36,9 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     delays = await asyncio.gather(*coroutines)
     return sorted(delays)
 
-if __name__ == "__main__":
-    wait_random = __import__('0-basic_async_syntax').wait_random
 
-    print(asyncio.run(wait_n(5, 5)))
-    print(asyncio.run(wait_n(10, 7)))
-    print(asyncio.run(wait_n(10, 0)))
+if __name__ == "__main__":
+    asyncio.run(wait_n(5, 5))
+    asyncio.run(wait_n(10, 7))
+    asyncio.run(wait_n(10, 0))
+
