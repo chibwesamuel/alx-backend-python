dd34492bc06ebe73c38b85c14cbc7b164667bcf3
chibwesamuel <smchibwe@gmail.com>
2023-09-14 14:43:30 +0000

    Cleaned this code

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-
+"""
+Return values with the appropriate types
+"""
 from typing import Iterable, Sequence, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Calculate the length of each element in a list.
@@ -10,7 +13,7 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
 
     Returns:
         List[Tuple[Sequence, int]]: A list of tuples where each tuple contains
-                                    the element from the input list and its length.
+                                    the element from the input list and its
+                                    length.
     """
     return [(i, len(i)) for i in lst]
-
