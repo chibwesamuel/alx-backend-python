bfea6a5973be22a5cb669841829a43e33b49e152
chibwesamuel <smchibwe@gmail.com>
2023-09-14 14:06:53 +0000

    Code cleanup

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""
+Takes a float n as argument and returns the floor of the float.
+"""
 import math
+
 
 def floor(n: float) -> int:
     """Return the floor value of a float.
