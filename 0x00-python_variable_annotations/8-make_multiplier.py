3e3ff765cf146498416e3fbd151ecd1547dfebba
chibwesamuel <smchibwe@gmail.com>
2023-09-14 14:37:27 +0000

    Code cleaup

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-
+"""
+Takes a float multiplier as argument and returns a function that multiplies
+a float by multiplier.
+"""
 from typing import Callable
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Create a multiplier function.
@@ -15,4 +19,3 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
         return value * multiplier
 
     return multiplier_function
-
