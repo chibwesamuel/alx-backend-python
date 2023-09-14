fb8a494c0f40c25478397dd878523fbf3bce5cd4
chibwesamuel <smchibwe@gmail.com>
2023-09-14 15:19:07 +0000

    Final cleanup

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,31 +2,31 @@
 """
 Validates a piece of code and applies changes
 """
-from typing import Tuple, List
+from typing import List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
     """
-    Zooms in on the given tuple by repeating its elements by a specified factor.
+    Zooms in on the given list by repeating its elements by a specified factor
 
     Args:
-        lst (Tuple[int, ...]): The input tuple containing integer elements to
-        be zoomed in on.
-        factor (int, optional): The factor by which the elements in the tuple
+        lst (List[int]): The input list containing integer elements to be
+        zoomed in on.
+        factor (int, optional): The factor by which the elements in the list
         will be repeated. Default is 2.
 
     Returns:
         List[int]: A list containing the zoomed-in elements.
 
     Example:
-        >>> array = (12, 72, 91)
+        >>> array = [12, 72, 91]
         >>> zoom_2x = zoom_array(array)
         >>> print(zoom_2x)
         [12, 12, 72, 72, 91, 91]
 
     Note:
-        This function takes a tuple of integers and repeats each element by
-        the specified factor to create a new list.
+        This function takes a list of integers and repeats each element by the
+        specified factor to create a new list.
     """
     zoomed_in: List[int] = [
         item for item in lst
@@ -34,8 +34,7 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     ]
     return zoomed_in
 
-
-array = (12, 72, 91)
+array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
