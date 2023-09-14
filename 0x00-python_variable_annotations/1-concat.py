89aa0ae861460cbcbf0229dececf59abf8b78f56
chibwesamuel <smchibwe@gmail.com>
2023-09-14 14:04:01 +0000

    New cleanup

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,5 +4,11 @@ Takes a string str1 and a string str2 as arguments and returns a
 concatenated string
 """
 
+
 def concat(str1: str, str2: str) -> str:
+    """
+    Puts two strings together to form one string
+
+    Returns: str
+    """
     return(str1 + str2)
